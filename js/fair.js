004cc51415f5ae62462c1a10e537652150b5c5d8
Jurij Ternicki <jurij@ternicki.com>
2016-04-01 11:30:59 +0300

    v2

---
+++ js/fair.js
@@ -0,0 +1,180 @@
+// Google Maps
+
+function init_map() {
+    var var_location = new google.maps.LatLng(50.424229, 30.543570);
+ 
+        var var_mapoptions = {
+          center: var_location,
+          zoom: 17,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          mapTypeControl: false,
+          panControl:false,
+          rotateControl:false,
+          streetViewControl: false,  
+          scrollwheel: false,
+          styles: 
+                [
+                    {
+                        "featureType": "all",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                            {
+                                "color": "#736c68"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "landscape.man_made",
+                        "elementType": "geometry.fill",
+                        "stylers": [
+                            {
+                                "color": "#e7e6e5"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "landscape.natural",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "visibility": "on"
+                            },
+                            {
+                                "color": "#d4e4d3"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi",
+                        "elementType": "geometry.fill",
+                        "stylers": [
+                            {
+                                "visibility": "on"
+                            },
+                            {
+                                "color": "#f5f5f5"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi.park",
+                        "elementType": "geometry.fill",
+                        "stylers": [
+                            {
+                                "color": "#d4e4d3"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "elementType": "geometry.fill",
+                        "stylers": [
+                            {
+                                "color": "#f5f5f5"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                            {
+                                "color": "#e7e6e5"
+                            },
+                            {
+                                "gamma": "0.65"
+                            },
+                            {
+                                "lightness": "0"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "transit",
+                        "elementType": "labels.text",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "water",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "color": "#aad5df"
+                            }
+                        ]
+                    }
+
+            ]
+        };
+
+    var var_pin = 'img/marker-21x32.png';
+    var var_marker = new google.maps.Marker({
+      position: var_location,
+      map: var_map,
+      icon: var_pin,
+      title:"DEC place"});
+ 
+        var var_map = new google.maps.Map(document.getElementById("map"),
+            var_mapoptions);
+ 
+    var_marker.setMap(var_map); 
+ 
+      }
+ 
+      google.maps.event.addDomListener(window, 'load', init_map);
+
+
+// Owl carousel
+$('.owl-portfolio').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: [
+          "<i class='fa fa-angle-left'></i>",
+          "<i class='fa fa-angle-right'></i>"
+          ],
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+// Owl carousel
+$('#owl-feedback').owlCarousel({
+    loop:true,
+    margin:10,
+    navigation : true, // Show next and prev buttons
+    dots:true,
+    autoplay:false,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+    
+});
