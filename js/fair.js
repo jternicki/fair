8b0d8453ba6d81afda8f84b817f8b173f3a2ba8e
jternicki <jurij@ternicki.com>
2016-04-08 12:50:58 +0300

    owl carousel fuckup

--- js/fair.js
+++ js/fair.js
@@ -158,7 +158,7 @@ $('.owl-portfolio').owlCarousel({
 $('.feedback-carousel').owlCarousel({
     loop:true,
     margin:10,
-    navigation : true, // Show next and prev buttons
+    navigation : false, // Show next and prev buttons
     dots:true,
     autoplay:false,
     autoplayTimeout:5000,
@@ -180,6 +180,37 @@ $('.feedback-carousel').owlCarousel({
     }
     
 });
+
+// Employers carousel 
+
+$('.employers-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    navigation : false, // Show next and prev buttons
+    nav: false,
+    navText: false,
+    dots: true,
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:true
+        }
+    }
+})
 
 
 // Copyright 2014-2015 Twitter, Inc.
